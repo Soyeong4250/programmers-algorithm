eb757e7fdef03ec751ea8ebb5eebbd996d59de69
이소영 <th42500@naver.com>
2022-03-01 21:32:27 +0900

    2022.03.01 study

---
+++ src/Hash/camouflage.java
@@ -0,0 +1,40 @@
+package Hash;
+
+import java.util.HashMap;
+
+public class camouflage { // 위장
+
+	public static void main(String[] args) {
+		String[][] clothes = { { "yellowhat", "headgear" }, { "bluesunglasses", "eyewear" },
+				{ "green_turban", "headgear" } };
+
+		System.out.println(solution(clothes));
+	}
+
+	private static int solution(String[][] clothes) {
+		// int answer = 0;
+
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		for (int i = 0; i < clothes.length; i++) {
+			String t = clothes[i][1];
+			map.put(t,  map.getOrDefault(t, 1) + 1);  // 착용을 안했을때까지 합쳐서 1부터 시작
+		}
+		
+		// 확인
+		for (String key : map.keySet()) {  
+			System.out.println(String.format("타입 : %s, 값: %d", key, map.get(key)));
+		}
+		
+		int answer = 1;
+		// 조합의 수 구하기
+		for (int cnt : map.values()) {
+			answer *= cnt;
+		}
+		
+		answer -= 1;  // 모두 착용x
+		
+		return answer;
+	}
+
+}
---
+++ src/Hash/marathon.java
@@ -0,0 +1,41 @@
+package Hash;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class marathon {  // 완주하지 못한 선수
+
+	public static void main(String[] args) {
+		// BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] participant = {"leo", "kiki", "eden"};
+		String[] completion = {"eden", "kiki"};
+		
+		System.out.println(solution(participant, completion));
+	}
+
+	private static String solution(String[] participant, String[] completion) {
+		String answer = "";
+		
+		Arrays.sort(participant);
+		Arrays.sort(completion);
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		
+		for (String p : participant) {
+			map.put(p, map.getOrDefault(p, 0) + 1);  // getOrDefault(가져와야 하는 요소의 키, 매핑된 값이 없는 경우 반환되어야 하는 기본값) 
+		}
+		for (String p : completion) {
+			map.put(p,  map.get(p) - 1);
+		}
+		
+		for (String key : map.keySet()) {  // map.keySet()
+			// System.out.println(String.format("선수명 : %s, 값: %d", key, map.get(key)));
+			if(map.get(key) != 0) {
+				answer = key;
+			}
+		}
+		return answer;
+	}
+
+}
