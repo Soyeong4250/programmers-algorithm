44924c0d9547f10b4034fa304bf6bc6ccc7a5f42
이소영 <th42500@naver.com>
2022-03-22 23:52:40 +0900

    update study

--- src/SampleQuestions/KakaoBlindRecruitment2.java
+++ src/SampleQuestions/KakaoBlindRecruitment2.java
@@ -1,58 +1,55 @@
 package SampleQuestions;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
-import java.util.StringTokenizer;
 
-public class KakaoBlindRecruitment2 { // 오픈채팅방
+public class KakaoBlindRecruitment2 { // 메뉴 리뉴얼
 
+	static HashMap<String, Integer> map = new HashMap<>();  // 각 조합의 개수를 파악할 hashmap
 	public static void main(String[] args) {
-		String[] record = { "Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan" };
-		System.out.println(Arrays.toString(solution(record)));
+		// String[] orders = { "ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH" };
+		String[] orders = {"XYZ", "XWY", "WXA"};
+		int[] course = { 2, 3, 4 };
+
+		solution(orders, course);
 	}
-	
-	private static String[] solution(String[] record) {
-		HashMap<String, String>map = new HashMap<>();
-		for (int i = 0; i < record.length; i++) {
-			StringTokenizer st  = new StringTokenizer(record[i]);
-			String command = st.nextToken();
-			String id = st.nextToken();
-			String nickName;
-			if(!command.equals("Leave")) {
-				nickName = st.nextToken();
-			} else {
-				nickName = null;
-			}
-			
-			if(command.equals("Enter")) {
-				map.put(id, nickName);
-			} else if(command.equals("Change")) {
-				map.put(id, nickName); 
-			}
+
+	private static void solution(String[] orders, int[] course) {
+		
+		// 1. 정렬하기
+		for (int i = 0; i < orders.length; i++) {
+			char[] ch = orders[i].toCharArray();
+			Arrays.sort(ch);
+			// 확인
+			// System.out.println(Arrays.toString(ch));
+			orders[i] = new String(ch);  // 정렬해서 문자열로 다시 삽입
 		}
 		
-		List<String> tmp = new ArrayList<>();
-		
-		for (int i = 0; i < record.length; i++) {
-			StringTokenizer st  = new StringTokenizer(record[i]);
-			String command = st.nextToken();
-			String id = st.nextToken();
-			
-			if(command.equals("Enter")) {
-				tmp.add(map.get(id) + "님이 들어왔습니다.");
-			}else if(command.equals("Leave")){
-				tmp.add(map.get(id)+ "님이 나갔습니다.");
+		// 2. 조합 뽑아내기
+		for (int i = 0; i < course.length; i++) {
+			for (int j = 0; j < orders.length; j++) {
+				System.out.println();
+				Combination("", orders[j], course[i]);  // j번째 문자열에서 course 길이만큼 조합 뽑기
 			}
 		}
-		
-		String[] answer = new String[tmp.size()];
-		for (int i = 0; i < answer.length; i++) {
-			answer[i] = tmp.get(i).toString();
-		}
-		
-		return answer;
 	}
+
+	private static void Combination(String str, String order, int cnt) {
+		// 기저조건
+		if(cnt == 0) {  // 다 골랐다
+			System.out.println("다 골랐습니다");
+			System.out.println(str);
+			map.put(str, map.getOrDefault(str, 0) + 1);  // hashmap에 조합 개수 + 1
+			return;
+		}
+		
+		for (int i = 0; i < order.length(); i++) {
+			System.out.println(order.charAt(i));
+			Combination(str + order.charAt(i), order.substring(i+1), cnt-1); 
+		}
+		
+	}
+
+
 
 }
