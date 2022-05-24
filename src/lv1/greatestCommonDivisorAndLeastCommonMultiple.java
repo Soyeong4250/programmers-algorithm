aa5b23461dc7da00bdd9644295e04b2b3f041dc0
이소영 <th42500@naver.com>
2022-05-24 23:17:03 +0900

    2022.05.24 study

---
+++ src/lv1/greatestCommonDivisorAndLeastCommonMultiple.java
@@ -0,0 +1,28 @@
+package lv1;
+
+public class greatestCommonDivisorAndLeastCommonMultiple { // 최대공약수와 최소공배수
+
+	public static void main(String[] args) {
+		int n = 3;
+		int m = 12;
+
+		System.out.println(solution(n, m));
+	}
+
+	public static int[] solution(int n, int m) {
+		int[] answer = {};
+		
+		// 유클리드 호제법 (n을 m으로 나눈 나머지가 0보다 클 때까지 반복)
+		int r = 0;
+		if(n < m) {
+			while(r!=0) {
+				r = m%n;
+			}
+		}
+		
+		
+		
+		return null;
+	}
+
+}
