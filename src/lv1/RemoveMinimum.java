89fd9bc9c62d4df59d0bcdfb05e89bfb3cfb7836
이소영 <th42500@naver.com>
2022-07-21 00:00:18 +0900

    2022.07.20 study

--- src/lv1/RemoveMinimum.java
+++ src/lv1/RemoveMinimum.java
@@ -3,8 +3,16 @@ package lv1;
 public class RemoveMinimum {  // 제일 작은 수 제거하기
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] arr = {4, 3, 2, 1};
+//		int[] arr = {10};
+		
+		System.out.println(solution(arr));
+	}
+	
+	private static int[] solution(int[] arr) {
+		int[] answer = {};
+		
+		return answer;
 	}
 
 }
