d51ad42bba95aba6c0afd80ec15c2ad357e4216d
이소영 <th42500@naver.com>
2022-07-24 23:30:22 +0900

    2022.07.24 study

---
+++ src/lv2/FindPrime.java
@@ -0,0 +1,27 @@
+package lv2;
+
+public class FindPrime {  // 소수 찾기
+
+	public static void main(String[] args) {
+		String numbers = "17";
+//		String numbers = "011";
+		
+		System.out.println(solution(numbers));
+	}
+
+	static int[] nums;
+	private static int solution(String numbers) {
+		int answer = 0;
+		
+		func(numbers);
+		
+		
+		return answer; 
+	}
+	
+	private static void func(String numbers) {
+		
+	}
+	
+	
+}
