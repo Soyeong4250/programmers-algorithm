b9c155402210c15d267873ad50d3f6861d1872c6
이소영 <th42500@naver.com>
2022-07-29 23:42:42 +0900

    2022.07.29 study-ing

--- src/lv2/NLeastCommonMultiple.java
+++ src/lv2/NLeastCommonMultiple.java
@@ -1,10 +1,29 @@
 package lv2;
 
-public class NLeastCommonMultiple {
+public class NLeastCommonMultiple { // N개의 최소공배수
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int[] arr = { 2, 6, 8, 14 };
+//		int[] arr = {1, 2, 3};
 
+		System.out.println(solution(arr));
+	}
+
+	private static int solution(int[] arr) {
+		int answer = 0;
+		
+		int gcd = GCD(arr);
+		
+		
+		
+		return answer;
+	}
+
+	private static int GCD(int[] arr) {
+		
+		
+		
+		return 0;
 	}
 
 }
