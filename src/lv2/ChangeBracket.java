5e3b79f239506a147bea0065b9c027b1a6a86f12
이소영 <th42500@naver.com>
2022-08-03 12:55:56 +0900

    2022.08.03 study

--- src/lv2/ChangeBracket.java
+++ src/lv2/ChangeBracket.java
@@ -1,17 +1,85 @@
 package lv2;
 
+import java.util.Stack;
+
 public class ChangeBracket { // 괄호 변환
 
 	public static void main(String[] args) {
-//		String p = "(()())()";
-		String p = ")(";
+		String p = "(()())()";
+//		String p = ")(";
 //		String p = "()))((()";
 
 		System.out.println(solution(p));
 	}
 
 	private static String solution(String p) {
+		String answer = "";
 		
+		// 1단계
+		if(p.length() == 0) {
+			return answer;
+		}
+		
+		// 2단계
+		StringBuilder u = new StringBuilder();
+		StringBuilder v = new StringBuilder();
+		int bracket = 0;
+		for(int i=0; i<p.length(); i++) {
+			u.append(p.charAt(i));
+			if(p.charAt(i) == '(') bracket++;
+			else bracket--;
+			
+			if(bracket == 0) {  // u가 균형잡힌 문자열이라면
+				v.append(p.substring(i+1));
+//				System.out.println("u는 " + u.toString());
+//				System.out.println("v는 " + v.toString());
+				break;
+			}
+		}
+		
+		// 3단계 & 4단계
+		if(isBalance(u.toString())) {  // u가 올바른 괄호 문자열이라면
+//			System.out.println("u는 " + u + "로 올바른 괄호 문자열입니다");
+			answer += u.toString();
+			answer += solution(v.toString());
+		} else {
+//			System.out.println("u는 " + u + "로 올바른 괄호 문자열이 아닙니다");
+			StringBuilder sb = new StringBuilder();
+			sb.append("(");
+			sb.append(solution(v.toString()));
+			sb.append(")");
+			for(int i=1; i< u.length()-1; i++) {
+				if(u.charAt(i) == '(') {
+					sb.append(')');
+				} else {
+					sb.append('(');
+				}
+			}
+			answer = sb.toString();
+		}
+		
+		return answer;
+	}
+
+	private static boolean isBalance(String u) {
+		Stack<Character> stack = new Stack<>();
+		for (int i = 0; i < u.length(); i++) {
+			char b = u.charAt(i);
+			if(stack.isEmpty() && b==')') {  // 첫 괄호가 닫는 괄호라면 올바른 괄호 x
+				return false;
+			}
+			if(!stack.isEmpty() && stack.peek() == '(' && b == ')') {
+				stack.pop();
+			} else {
+				stack.push(b);
+			}
+		}
+		
+//		System.out.println(stack);
+		if(!stack.isEmpty()) {  // 스택이 비어있지 않다면 올바른 괄호 x
+			return false;
+		}
+		return true;
 	}
 
 }
--- src/lv2/NLeastCommonMultiple.java
+++ src/lv2/NLeastCommonMultiple.java
@@ -1,29 +1,48 @@
 package lv2;
 
+import java.util.Arrays;
+
 public class NLeastCommonMultiple { // N개의 최소공배수
 
 	public static void main(String[] args) {
-		int[] arr = { 2, 6, 8, 14 };
+//		int[] arr = { 2, 6, 8, 14 };
 //		int[] arr = {1, 2, 3};
+		int[] arr = {3, 4, 9, 16};
 
 		System.out.println(solution(arr));
 	}
 
 	private static int solution(int[] arr) {
 		int answer = 0;
+		long gcd = 0;
+		long multiple = 0;
 		
-		int gcd = GCD(arr);
+		Arrays.sort(arr);
 		
+		// 최대공약수 구하기 (유클리드 호제법)
+		for(int i = 1;i < arr.length; i++) {
+			if(gcd == 0) {
+				gcd = GCD(arr[i], arr[i-1], gcd);
+				multiple = arr[i] * arr[i-1];
+			} else {
+				gcd *= GCD(multiple/gcd, arr[i], gcd);
+				multiple *= arr[i];
+			}
+//			System.out.println("multiple은 " + multiple + ", gcd는 " + gcd);
+		}
 		
+		answer = (int)multiple/(int)gcd;
 		
 		return answer;
 	}
 
-	private static int GCD(int[] arr) {
-		
-		
-		
-		return 0;
+	private static long GCD(long a, long b, long gcd) {
+		if(b == 0) {
+			gcd = a;
+			return gcd;
+		} else {
+			return GCD(b, a%b, gcd);
+		}
 	}
 
 }
