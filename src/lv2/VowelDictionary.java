2b57b575ff5e0ad9e4e26ce957bafc6aec8fab3b
이소영 <th42500@naver.com>
2022-08-07 23:58:24 +0900

    2022.08.07 study

---
+++ src/lv2/VowelDictionary.java
@@ -0,0 +1,22 @@
+package lv2;
+
+public class VowelDictionary { // 모음사전
+
+	public static void main(String[] args) {
+		String word = "AAAAE";
+//		String word = "AAAE";
+//		String word = "I";
+//		String word = "EIO";
+		System.out.println(solution(word));
+	}
+
+	private static int solution(String word) {
+		int answer = 0;
+		
+		
+		
+		
+		return null;
+	}
+
+}
