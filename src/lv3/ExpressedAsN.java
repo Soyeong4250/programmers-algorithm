1dc79423e7e9d645c0f91eec4e20a14b5abfa4ed
이소영 <th42500@naver.com>
2022-08-11 23:57:17 +0900

    2022.08.11 study

---
+++ src/lv3/ExpressedAsN.java
@@ -0,0 +1,27 @@
+package lv3;
+
+public class ExpressedAsN { // N으로 표현
+
+	public static void main(String[] args) {
+		int N = 5;
+		int number = 12;
+
+//		int N = 2;
+//		int number = 11;
+
+		System.out.println(solution(N, number));
+	}
+
+	private static int solution(int n, int number) {
+		int answer = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			
+		}
+		
+		return answer;
+	}
+
+}
