b0e5b52c34df12b2f692e9420e032553b54d850d
이소영 <th42500@naver.com>
2022-08-29 23:55:16 +0900

    2022.08.29 study

--- src/lv1/CheckingPersonalityTypes.java
+++ src/lv1/CheckingPersonalityTypes.java
@@ -12,13 +12,72 @@ public class CheckingPersonalityTypes { // 성격 유형 검사하기
 		System.out.println(solution(survey, choices));
 	}
 
+	class Types {
+		int rs;
+		int ts;
+		int cs;
+		int fs;
+		int js;
+		int ms;
+		int as;
+		int ns;
+		
+		public Types(int rs, int ts, int cs, int fs, int js, int ms, int as, int ns) {
+			this.rs = rs;
+			this.ts = ts;
+			this.cs = cs;
+			this.fs = fs;
+			this.js = js;
+			this.ms = ms;
+			this.as = as;
+			this.ns = ns;
+		}
+	}
+	
 	private static String solution(String[] survey, int[] choices) {
 		String answer = "";
-		String[] types = new String[4];
+		int r = 0;
+		int t = 0;
+		int c = 0;
+		int f = 0;
+		int j = 0;
+		int m = 0;
+		int a = 0;
+		int n = 0;
+		
 		
 		for (int i = 0; i < choices.length; i++) {
 			if(choices[i] <= 3) {  // 동의일 경우
-				String type = survey[i].substring(0, 1);  // 앞의 성격 유형 선택
+				String type = survey[i].substring(0, 1).toLowerCase();  // 앞의 성격 유형 선택
+				switch (type) {
+				case "r":
+					
+					break;
+				case "t":
+					
+					break;
+				case "c":
+					
+					break;
+				case "f":
+					
+					break;
+				case "j":
+					
+					break;
+				case "m":
+					
+					break;
+				case "a":
+					
+					break;
+				case "n":
+					
+					break;
+				}
+				
+			} else if (choices[i] >= 5) {  // 비동의일 경우
+				String type = survey[i].substring(1, 2);  // 뒤의 성격 유형 선택
 				
 			}
 		}
