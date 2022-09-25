2801eff24bec361ed9324321aa59c792e92273a5
이소영 <th42500@naver.com>
2022-09-25 14:22:39 +0900

    2022.09.25 H-Index

--- src/lv2/HIndex.java
+++ src/lv2/HIndex.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class HIndex {
 
 	public static void main(String[] args) {
-		int[] citations = {3, 0, 6, 1, 5};
+//		int[] citations = {3, 0, 6, 1, 5};
+		int[] citations = {9, 2, 5, 1};
+//		int[] citations = {6, 5, 5, 5, 3, 2, 1, 0};
+//		int[] citations = {0, 1};
 		System.out.println(solution(citations));
 	}
 	
@@ -13,14 +16,16 @@ public class HIndex {
 		int answer = 0;
 		
 		Arrays.sort(citations);
+		System.out.println(Arrays.toString(citations));
 		
-//		System.out.println(Arrays.toString(citations));
-		
-		for(int i=0; i<citations.length; i++) {
-			if(citations[i] >= i+1) {
-				answer = i+1;
+		for (int i = 0; i < citations.length; i++) {
+			int hIdx = citations.length - i;
+			
+			if(citations[i] >= hIdx) {
+				answer = hIdx;
 				break;
 			}
+			
 		}
 		
 		
