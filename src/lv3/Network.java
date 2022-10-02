a38fac48c0f2c456b34fa1d24d9229b3f4d07779
이소영 <th42500@naver.com>
2022-10-02 10:48:25 +0900

    2022.10.02 study

--- src/lv3/Network.java
+++ src/lv3/Network.java
@@ -1,7 +1,5 @@
 package lv3;
 
-import java.util.HashMap;
-
 public class Network { // 네트워크
 
 	public static void main(String[] args) {
@@ -13,54 +11,29 @@ public class Network { // 네트워크
 		System.out.println(solution(n, computers));
 	}
 
-	static int[] parents;
 	private static int solution(int n, int[][] computers) {
-
 		int answer = 0;
-		parents = new int[n];
+		boolean[] v = new boolean[n]; // n 갯수만큼 boolean 배열을 만들고 모든 요소를 false로 초기화
 
-		// 첫 부모는 자기 자신
-		for (int i = 0; i < parents.length; i++) {
-			parents[i] = i;
-		}
-
-		// union
-		for (int i = 0; i < computers.length; i++) {
-			for (int j = i; j < computers[i].length; j++) {
-				if (computers[i][j] == 1) { // 1이라면 연결되어 있음
-					union(i, j);
-				}
+		for (int i = 0; i < n; i++) {
+			if (!v[i]) {
+				dfs(computers, i, v);
+				answer++;
 			}
 		}
 
-		HashMap<Integer, Integer> hash = new HashMap<>();
-		for (int i = 0; i < parents.length; i++) {
-			hash.put(parents[i], hash.getOrDefault(parents[i], 0) + 1);
-		}
-		answer = hash.size();
 		return answer;
 	}
 
-	private static void union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		// 같은 부모를 가지고 있지 않다면
-		if (x != y) {
-			if (x < y)
-				parents[y] = x;
-			else
-				parents[x] = y;
-		}
+	private static boolean[] dfs(int[][] computers, int idx, boolean[] v) {
+		v[idx] = true;
 
-	}
-
-	private static int find(int x) {
-		// 부모 배열의 부모가 자기 자신이라면 return
-		if (x == parents[x])
-			return x;
-		// 부모 배열의 부모가 자기 자신이 아니라면 parents[x]의 부모 찾기
-		else
-			return parents[x] = find(parents[x]);
+	    for (int j = 0; j < computers.length; j++) {
+	      if (idx != j && computers[idx][j] == 1 && v[j] == false) {
+	        v = dfs(computers, j, v);
+	      }
+	    }
+	    return v;
 	}
 
 }
