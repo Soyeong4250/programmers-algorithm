72cc9e8459471d2d34a10406594787788b9a4c2f
이소영 <th42500@naver.com>
2022-10-15 20:43:15 +0900

    Refactoring 모의고사

--- src/lv1/trialExamination.java
+++ src/lv1/trialExamination.java
@@ -18,7 +18,6 @@ public class TrialExamination { // 모의고사
 		int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
 		int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 		
-		ArrayList<Integer> list = new ArrayList<>();
 		int[] success = new int[3];
 		for (int i = 0; i < answers.length; i++) {
 			if(answers[i] == one[i%one.length]) {
@@ -32,10 +31,11 @@ public class TrialExamination { // 모의고사
 			}
 		}
 		
-		int max = 0;
-		for (int i = 0; i < success.length; i++) {
-			if(max < success[i]) max = success[i];
-		}
+		ArrayList<Integer> list = new ArrayList<>();  // 많이 맞춘 수포자가 여러명일 수 있으므로 수포자를 담을 List 생성하기
+		int max = Math.max(Math.max(success[0], success[1]), success[2]);
+//		for (int i = 0; i < success.length; i++) {
+//			if(max < success[i]) max = success[i];
+//		}
 		
 		for (int i = 0; i < success.length; i++) {
 			if(success[i]  == max) {
