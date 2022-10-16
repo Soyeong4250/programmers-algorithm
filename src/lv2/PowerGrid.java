b25ee970470f798a1ff96395cdfd6422c42257f7
이소영 <th42500@naver.com>
2022-10-16 19:41:35 +0900

    Style PowerGrid.java
    1. 전력망을 둘로 나누기 다시 풀어보기 👉 변수명 수정

--- src/lv2/PowerGrid.java
+++ src/lv2/PowerGrid.java
@@ -8,8 +8,6 @@ import java.util.Queue;
 public class PowerGrid { // 전력망을 둘로 나누기
 
 	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
 		int n = 9;
 		int[][] wires = { { 1, 3 }, { 2, 3 }, { 3, 4 }, { 4, 5 }, { 4, 6 }, { 4, 7 }, { 7, 8 }, { 7, 9 } };
 
@@ -18,53 +16,52 @@ public class PowerGrid { // 전력망을 둘로 나누기
 
 //		int n = 7;
 //		int[][] wires = {{1, 2}, {2, 7}, {3, 7}, {3, 4}, {4, 5}, {6, 7}};
+		PowerGrid powerGrid = new PowerGrid();
 
-		System.out.println(solution(n, wires));
+		System.out.println(powerGrid.solution(n, wires));
 
 	}
 	
-	private static int solution(int n, int[][] wires) {
+	private int solution(int n, int[][] wires) {
 		int answer = Integer.MAX_VALUE;
 		
-		// 인접리스트
-		int[][] adj = new int[n+1][n+1];
+		int[][] adj = new int[n+1][n+1];  // 인덱스는 0부터 시작, 전력망은 1부터 시작하므로 인접행렬 길이 +1
 		
-		// 양방향 입력
 		for (int i = 0; i < wires.length; i++) {
-			adj[wires[i][0]][wires[i][1]] = 1;
-			adj[wires[i][1]][wires[i][0]] = 1;			
+			adj[wires[i][0]][wires[i][1]] = adj[wires[i][1]][wires[i][0]] = 1;  // 양방향
 		}
 		
-		// 연결된 에지 하나씩 끊어보며 탐색
+		// 하나씩 끊어보기
 		for (int i = 0; i < wires.length; i++) {
-			int s = wires[i][0];
-			int e = wires[i][1];
+			int sNode = wires[i][0];
+			int eNode = wires[i][1];
 			
-			// 에지 끊기
-			adj[s][e] = 0;
-			adj[e][s] = 0;
+			// 연결된 전력망 끊기
+			adj[sNode][eNode] = 0;
+			adj[eNode][sNode] = 0;
 			
 			answer = Math.min(answer, bfs(adj, new boolean[n+1], 1, n));
 			
-			// 에지 원상복귀
-			adj[s][e] = 1;
-			adj[e][s] = 1;
+			// 연결된 전력망 연결
+			adj[sNode][eNode] = 1;
+			adj[eNode][sNode] = 1;
 		}
 		
 		return answer;
 	}
-	
-	private static int bfs(int[][] adj, boolean[] v, int start, int n) {		
+
+	private int bfs(int[][] adj, boolean[] v, int sNode, int n) {
 		Queue<Integer> q = new LinkedList<>();
-		q.offer(start);
-		int cnt = 1;
+		
+		q.offer(sNode);
+		int cnt = 1;  // 개수 1 더하기(1번 노드)
 		
 		while(!q.isEmpty()) {
 			int node = q.poll();
 			v[node] = true;
 			
 			for (int i = 1; i < v.length; i++) {
-				if(!v[i] && adj[node][i] == 1) {  // 방문한적 없는 노드이고 현재 노드와 연결되어 있다면
+				if(!v[i] && adj[node][i] == 1) {
 					q.offer(i);
 					cnt++;
 				}
@@ -73,5 +70,6 @@ public class PowerGrid { // 전력망을 둘로 나누기
 		
 		return (int)Math.abs(cnt - (n - cnt));
 	}
+	
 
 }
