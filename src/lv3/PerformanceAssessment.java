3ce07aff0d17d6a1069f0880cf23f23be27ccd72
이소영 <th42500@naver.com>
2023-03-10 00:20:26 +0900

    [Fix] 2023.03.10 - 인사고과 풀이 진행중
    👉 테스트 케이스 8, 10, 11, 14, 16 실패
    👉 테스트 케이스 21, 22, 24, 25 시간 초과
    👉 정확성 64.0

--- src/lv3/PerformanceAssessment.java
+++ src/lv3/PerformanceAssessment.java
@@ -2,8 +2,6 @@ package lv3;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 
 public class PerformanceAssessment { // 인사고과
 
@@ -11,37 +9,38 @@ public class PerformanceAssessment { // 인사고과
 		PerformanceAssessment main = new PerformanceAssessment();
 
 //		int[][] scores = { { 2, 2 }, { 1, 4 }, { 3, 2 }, { 3, 2 }, { 2, 1 }};
-		int[][] scores = {{1, 1}, {5, 3}, {2, 4}, {1, 4}};
-//		int[][] scores = {{2, 2}, {2, 2}, {3, 2}, {2, 3}};
+//		int[][] scores = {{1, 1}, {5, 3}, {2, 4}, {1, 4}};
+		int[][] scores = {{2, 2}, {2, 2}, {3, 2}, {2, 3}};
 		System.out.println(main.solution(scores));
 	}
 
 	private int solution(int[][] scores) {
-		int wanhoScore = scores[0][0] + scores[0][1];
-		
-		HashMap<Integer, Integer> scoreHash = new HashMap<>();
-		
-		for (int i = 1; i < scores.length; i++) {
-			if(scores[0][0] < scores[i][0] && scores[0][1] < scores[i][1]) {
-				return -1;
+		// -1 판별
+		for (int i = 0; i < scores.length-1; i++) {
+			for (int j = i+1; j < scores.length; j++) {
+				if(scores[i][0] < scores[j][0] && scores[i][1] < scores[j][1]) {
+					scores[i][0] = -1;
+					scores[i][1] = -1;
+					
+					if(i == 0) {
+						return -1;
+					}
+				}
 			}
-			
-			int sum = scores[i][0] + scores[i][1];
-			scoreHash.put(sum, scoreHash.getOrDefault(sum, 0) + 1);
 		}
 		
-		List<Integer> keyList = new ArrayList<>(scoreHash.keySet());
-		keyList.sort((s1, s2) -> s2-s1);
-		
-		System.out.println(keyList);
-		
-		int grade = 0;
-		for (int key : keyList) {
-			if(key == wanhoScore) {
-				grade += 1;
+		int wanho = scores[0][0] + scores[0][1];
+		int[] sum = new int[scores.length];
+		for (int i = 1; i < scores.length; i++) {
+			sum[i] = scores[i][0] + scores[i][1];
+		}
+		Arrays.sort(sum);
+		int grade = 1;
+		for (int i = sum.length-1; i >= 0; i--) {
+			if(sum[i] > wanho) {
+				grade++;
+			}else if(sum[i] == wanho) {
 				break;
-			}else {
-				grade += scoreHash.get(key);
 			}
 		}
 		
