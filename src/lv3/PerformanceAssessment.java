fb00e1d5d0b4d27c49b7ecbe1b7d161e5e063a80
이소영 <th42500@naver.com>
2023-03-10 00:06:04 +0900

    [Fix] 2023.03.09 - 인사고과 풀이 진행중
    👉 테스트 케이스 1, 3, 5, 7, 8, 10, 11, 12, 14, 16, 17, 18, 22 실패 (정확성 48.0)

--- src/lv3/PerformanceAssessment.java
+++ src/lv3/PerformanceAssessment.java
@@ -1,6 +1,9 @@
 package lv3;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class PerformanceAssessment { // 인사고과
 
@@ -8,43 +11,37 @@ public class PerformanceAssessment { // 인사고과
 		PerformanceAssessment main = new PerformanceAssessment();
 
 //		int[][] scores = { { 2, 2 }, { 1, 4 }, { 3, 2 }, { 3, 2 }, { 2, 1 }};
-//		int[][] scores = {{1, 1}, {5, 3}, {2, 4}, {1, 4}};
-		int[][] scores = {{2, 2}, {2, 2}, {3, 2}, {2, 3}};
+		int[][] scores = {{1, 1}, {5, 3}, {2, 4}, {1, 4}};
+//		int[][] scores = {{2, 2}, {2, 2}, {3, 2}, {2, 3}};
 		System.out.println(main.solution(scores));
 	}
 
 	private int solution(int[][] scores) {
 		int wanhoScore = scores[0][0] + scores[0][1];
 		
-		int[] sum = new int[scores.length];
-		int min1 = 100001;
-		int min2 = 100001;
+		HashMap<Integer, Integer> scoreHash = new HashMap<>();
 		
-		for (int i = 0; i < sum.length; i++) {
-			sum[i] = scores[i][0] + scores[i][1];
+		for (int i = 1; i < scores.length; i++) {
 			if(scores[0][0] < scores[i][0] && scores[0][1] < scores[i][1]) {
 				return -1;
 			}
+			
+			int sum = scores[i][0] + scores[i][1];
+			scoreHash.put(sum, scoreHash.getOrDefault(sum, 0) + 1);
 		}
 		
-		if(scores[0][0] == min1 && scores[0][1] == min2) {
-			return -1;
-		}
+		List<Integer> keyList = new ArrayList<>(scoreHash.keySet());
+		keyList.sort((s1, s2) -> s2-s1);
 		
-		Arrays.sort(sum);
-		System.out.println(Arrays.toString(sum));
+		System.out.println(keyList);
 		
-		int prev = sum[0];
-		int grade = 1;
-		for (int i = sum.length-1; i >= 0; i--) {
-			if(sum[i] == wanhoScore) {
+		int grade = 0;
+		for (int key : keyList) {
+			if(key == wanhoScore) {
+				grade += 1;
 				break;
-			}
-			if(prev == sum[i]) {
-				grade++;
-			} else {
-				prev = sum[i];
-				grade++;
+			}else {
+				grade += scoreHash.get(key);
 			}
 		}
 		
