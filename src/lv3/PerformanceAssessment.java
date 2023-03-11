7888a1e1357d2488ed37f9f5fc5d2aebcc8f310b
이소영 <th42500@naver.com>
2023-03-12 00:05:12 +0900

    [Feat] 2023.03.12 - 인사고과 풀이완료

--- src/lv3/PerformanceAssessment.java
+++ src/lv3/PerformanceAssessment.java
@@ -9,15 +9,15 @@ public class PerformanceAssessment { // 인사고과
 		PerformanceAssessment main = new PerformanceAssessment();
 
 //		int[][] scores = { { 2, 2 }, { 1, 4 }, { 3, 2 }, { 3, 2 }, { 2, 1 }};  // 4
-//		int[][] scores = {{1, 1}, {5, 3}, {2, 4}, {1, 4}};  // -1
-		int[][] scores = {{3, 2}, {2, 4}, {0, 1}, {1, 4}, {3, 6}};  // 2
+		int[][] scores = {{1, 1}, {5, 3}, {2, 4}, {1, 4}};  // -1
+//		int[][] scores = {{3, 2}, {2, 4}, {0, 1}, {1, 4}, {3, 6}};  // 2
 //		int[][] scores = {{2, 2}, {2, 2}, {3, 2}, {2, 3}};  // 3
 		System.out.println(main.solution(scores));
 	}
 
 	private int solution(int[][] scores) {
 		int[] wanhoScore = scores[0];
-		System.out.println(Arrays.toString(wanhoScore));
+//		System.out.println(Arrays.toString(wanhoScore));
 		
 		Arrays.sort(scores, new Comparator<int[]>(){
 			@Override
@@ -30,13 +30,13 @@ public class PerformanceAssessment { // 인사고과
 			}
 		});
 		
-		for (int i = 0; i < scores.length; i++) {
-			System.out.println(Arrays.toString(scores[i]));			
-		}
+//		for (int i = 0; i < scores.length; i++) {
+//			System.out.println(Arrays.toString(scores[i]));			
+//		}
 		
 		int wanhoSum = wanhoScore[0] + wanhoScore[1];
 		int peerScore = 0;
-		int grade = 0;
+		int grade = 1;
 		for (int i = 0; i < scores.length; i++) {
 			if(scores[i][1] < peerScore) {
 				if(wanhoScore[0] == scores[i][0] && wanhoScore[1] == scores[i][1]) {
