66895b090eec7a393edd50583778a42c6a70fdbc
이소영 <th42500@naver.com>
2023-03-13 20:05:39 +0900

    [Feat] 2023.03.13 - 섬 연결하기 풀이 완료

--- src/lv3/LinkIsland.java
+++ src/lv3/LinkIsland.java
@@ -7,15 +7,17 @@ public class LinkIsland { // 섬 연결하기
 	public static void main(String[] args) {
 		LinkIsland main = new LinkIsland();
 
-//		int n = 4;
-//		int[][] costs = { { 0, 1, 1 }, { 0, 2, 2 }, { 1, 2, 5 }, { 1, 3, 1 }, { 2, 3, 8 } };
-		int n = 5;
-		int[][] costs = { { 0, 1, 1 }, { 1, 2, 4 }, { 1, 3, 1 }, { 2, 3, 2 }, { 3, 4, 3 } };
+		int n = 4;
+		int[][] costs = { { 0, 1, 1 }, { 0, 2, 2 }, { 1, 2, 5 }, { 1, 3, 1 }, { 2, 3, 8 } };
+//		int n = 5;
+//		int[][] costs = { { 0, 1, 1 }, { 1, 2, 4 }, { 1, 3, 1 }, { 2, 3, 2 }, { 3, 4, 3 } };
+//		int n = 6;
+//		int[][] costs = { {0, 1, 5}, {0, 3, 2}, {0, 4, 3}, {1, 4, 1}, {3, 4, 10}, {1, 2, 2}, {2, 5, 3}, {4, 5, 4} };
 		System.out.println(main.solution(n, costs));
 	}
 
 	private int solution(int n, int[][] costs) {
-		Arrays.sort(costs, (s1, s2) -> s1[2] == s2[2] ? s1[0] - s2[0] : s1[2] - s2[2]);
+		Arrays.sort(costs, (s1, s2) -> s1[2] - s2[2]);
 		
 		// 정렬 확인
 //		for (int i = 0; i < costs.length; i++) {
@@ -32,10 +34,21 @@ public class LinkIsland { // 섬 연결하기
 			if(parent[costs[i][0]] != parent[costs[i][1]]) {
 				answer += costs[i][2];
 //				System.out.println(costs[i][2]);
+				int from;
+				int to;
 				if(parent[costs[i][0]] < parent[costs[i][1]]) {
+					from = parent[costs[i][1]];
+					to = parent[costs[i][0]];
 					parent[costs[i][1]] = parent[costs[i][0]];
 				} else {
+					from = parent[costs[i][0]];
+					to = parent[costs[i][1]];
 					parent[costs[i][0]] = parent[costs[i][1]];
+				}
+				for (int j = 0; j < parent.length; j++) {
+					if(parent[j] == from) {
+						parent[j] = to;
+					}
 				}
 			}
 //			System.out.println(Arrays.toString(parent));
