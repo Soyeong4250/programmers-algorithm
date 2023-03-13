144e0b34737aef5b72e4cdc6aa23feb178dd5bbc
이소영 <th42500@naver.com>
2023-03-13 20:20:25 +0900

    [Refactor] 2023.03.13 - 섬 연결하기 코드 수정
    👉 Union-find로 풀이 정리

--- src/lv3/LinkIsland.java
+++ src/lv3/LinkIsland.java
@@ -7,12 +7,12 @@ public class LinkIsland { // 섬 연결하기
 	public static void main(String[] args) {
 		LinkIsland main = new LinkIsland();
 
-		int n = 4;
-		int[][] costs = { { 0, 1, 1 }, { 0, 2, 2 }, { 1, 2, 5 }, { 1, 3, 1 }, { 2, 3, 8 } };
+//		int n = 4;
+//		int[][] costs = { { 0, 1, 1 }, { 0, 2, 2 }, { 1, 2, 5 }, { 1, 3, 1 }, { 2, 3, 8 } };
 //		int n = 5;
 //		int[][] costs = { { 0, 1, 1 }, { 1, 2, 4 }, { 1, 3, 1 }, { 2, 3, 2 }, { 3, 4, 3 } };
-//		int n = 6;
-//		int[][] costs = { {0, 1, 5}, {0, 3, 2}, {0, 4, 3}, {1, 4, 1}, {3, 4, 10}, {1, 2, 2}, {2, 5, 3}, {4, 5, 4} };
+		int n = 6;
+		int[][] costs = { {0, 1, 5}, {0, 3, 2}, {0, 4, 3}, {1, 4, 1}, {3, 4, 10}, {1, 2, 2}, {2, 5, 3}, {4, 5, 4} };
 		System.out.println(main.solution(n, costs));
 	}
 
@@ -31,30 +31,28 @@ public class LinkIsland { // 섬 연결하기
 		
 		int answer = 0;
 		for (int i = 0; i < costs.length; i++) {
-			if(parent[costs[i][0]] != parent[costs[i][1]]) {
-				answer += costs[i][2];
+			if(find(parent, costs[i][0]) != find(parent, costs[i][1])) {
 //				System.out.println(costs[i][2]);
-				int from;
-				int to;
-				if(parent[costs[i][0]] < parent[costs[i][1]]) {
-					from = parent[costs[i][1]];
-					to = parent[costs[i][0]];
-					parent[costs[i][1]] = parent[costs[i][0]];
-				} else {
-					from = parent[costs[i][0]];
-					to = parent[costs[i][1]];
-					parent[costs[i][0]] = parent[costs[i][1]];
-				}
-				for (int j = 0; j < parent.length; j++) {
-					if(parent[j] == from) {
-						parent[j] = to;
-					}
-				}
+				parent = union(parent, costs[i][0], costs[i][1]);
+				answer += costs[i][2];
 			}
 //			System.out.println(Arrays.toString(parent));
 		}
 		
 		return answer;
+	}
+
+	private int find(int[] parent, int i) {
+		if(parent[i] == i)  return i;
+		return find(parent, parent[i]);
+	}
+
+	private int[] union(int[] parent, int i, int j) {
+		int parent1 = find(parent, i);
+		int parent2 = find(parent, j);
+		
+		parent[parent2] = parent1;
+		return parent;
 	}
 
 }
