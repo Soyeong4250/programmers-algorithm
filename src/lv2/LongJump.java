6c1348ea7a6f7cee131d7827f5b45083bbeef090
이소영 <th42500@naver.com>
2023-03-16 23:47:52 +0900

    [Feat] 2023.03.16 - 멀리뛰기 풀이중 테스트 1, 2번 실패

---
+++ src/lv2/LongJump.java
@@ -0,0 +1,31 @@
+package lv2;
+
+public class LongJump { // 멀리뛰기
+
+	public static void main(String[] args) {
+		LongJump main = new LongJump();
+
+		int n = 1;
+//		int n = 4;
+//		int n = 3;
+
+		System.out.println(main.solution(n));
+	}
+
+	private long solution(int n) {
+		long answer = 0;
+		
+		long first = 1;
+		long second = 2;
+		
+		
+		for (int i = 3; i <= n; i++) {
+			answer = (first + second)%1234567;
+			first = second;
+			second = answer;
+		}
+		
+		return answer%1234567;
+	}
+
+}
