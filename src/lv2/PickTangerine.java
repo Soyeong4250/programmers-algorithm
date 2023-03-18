39d4536b10272cac06ffc3f54fbfb58d09200e0c
이소영 <th42500@naver.com>
2023-03-18 22:04:47 +0900

    [Feat] 2023.03.18 - 귤 고르기 풀이완료

--- src/lv2/PickTangerine.java
+++ src/lv2/PickTangerine.java
@@ -1,20 +1,24 @@
 package lv2;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 public class PickTangerine { // 굴 고르기
 
 	public static void main(String[] args) {
 		PickTangerine main = new PickTangerine();
 
-//		int k = 6;
-//		int[] tangerine = { 1, 3, 2, 5, 4, 5, 2, 3 };
+		int k = 6;
+		int[] tangerine = { 1, 3, 2, 5, 4, 5, 2, 3 };
 
 //		int k = 4;
 //		int[] tangerine = {1, 3, 2, 5, 4, 5, 2, 3};
 
-		int k = 2;
-		int[] tangerine = {1, 1, 1, 1, 2, 2, 2, 3};
+//		int k = 2;
+//		int[] tangerine = {1, 1, 1, 1, 2, 2, 2, 3};
 
 		System.out.println(main.solution(k, tangerine));
 	}
@@ -28,34 +32,40 @@ public class PickTangerine { // 굴 고르기
 			hash.put(tangerine[i], hash.getOrDefault(tangerine[i], 0) + 1);
 		}
 		
-		answer = pick(tangerine.length - k, tangerine, new boolean[tangerine.length], hash, hash.size());
+		answer = pick(tangerine.length - k, tangerine, hash);
 		
 		return answer;
 	}
 
-	private int pick(int k, int[] tangerine, boolean[] check, HashMap<Integer, Integer> hash, int min) {
-//		System.out.println(k);
-		if(k <= 0) {
-//			System.out.println("k가 0이하" + hash);
-			if(min > hash.size()) {
-//				System.out.println("min 변경 = " + hash.size());
-				min = hash.size();
+	private int pick(int k, int[] tangerine, HashMap<Integer, Integer> hash) {
+//		System.out.println("k = " + k);
+		
+		// HashMap value 기준 정렬
+		List<Map.Entry<Integer, Integer>> entries = hash.entrySet().stream()
+				.sorted(Map.Entry.comparingByValue())
+				.collect(Collectors.toList());
+		
+//		for (Entry<Integer, Integer> entry : entries) {
+//			System.out.println(entry.getKey() + ":" + entry.getValue());
+//		}
+		
+		for (Entry<Integer, Integer> entry : entries) {
+			if(entry.getValue() <= k) {
+				k -= entry.getValue();
+				hash.remove(entry.getKey());
+//				System.out.println(k);
 			}
-			return min;
+			
+			if(k == 0) {
+				break;
+			}
 		}
 		
-		for (int i = 0; i < tangerine.length; i++) {
-			if(!check[i]) {
-				check[i] = true;
-				hash.put(tangerine[i], hash.get(tangerine[i])-1);
-				if(hash.get(tangerine[i]) == 0) hash.remove(tangerine[i]);
-				min = pick(k-1, tangerine, check, hash, min);
-				hash.put(tangerine[i], hash.getOrDefault(tangerine[i], 0) + 1);
-				check[i] = false;
-			}
-		}
+//		for (Entry entry : hash.entrySet()) {
+//			System.out.println(entry.getKey() + ":" + entry.getValue());
+//		}
 		
-		return min;
+		return hash.size();
 	}
 
 }
