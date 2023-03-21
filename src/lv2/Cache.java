081650de3f3f80bd26e69c128fa8fa43c8d2f49c
이소영 <th42500@naver.com>
2023-03-22 03:22:24 +0900

    [Refactor] 2023.03.22 - 캐시 코드 수정 (사용하지 않는 라이브러리 제거)

--- src/lv2/Cache.java
+++ src/lv2/Cache.java
@@ -1,10 +1,7 @@
 package lv2;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Cache { // 캐시
 
