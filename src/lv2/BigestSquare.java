c2fc2b481d9ccf51f2ecebe55f23e323b71f8324
이소영 <th42500@naver.com>
2023-03-24 12:48:35 +0900

    [Feat] 2023.03.24 - 가장 큰 정사각형 찾기 풀이중
    👉 테스트케이스 4, 6, 9, 10, 12~19 실패
    👉 효율성 테스트 모두 실패 (시간초과 해결)

--- src/lv2/BigestSquare.java
+++ src/lv2/BigestSquare.java
@@ -7,18 +7,21 @@ public class BigestSquare { // 가장 큰 정사각형 찾기
 		int[][] board = { { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 0, 1, 0 } };
 //		int[][] board = {{0, 0, 1, 1}, {1, 1, 1, 1}};
 
+		long start = System.currentTimeMillis();
 		System.out.println(main.solution(board));
+		long end = System.currentTimeMillis();
+		System.out.println("걸린 시간 = " + (end - start));
 	}
 
+	static boolean[][] v;
 	private int solution(int[][] board) {
 		int answer = 0;
 		
 		int line = 0;
-		
-//		boolean[][] v = new boolean[board.length][board[0].length];
+		v = new boolean[board.length][board[0].length];
 		for (int i = 0; i < board.length; i++) {
 			for (int j = 0; j < board[i].length; j++) {
-				if(board[i][j] == 1) {
+				if(board[i][j] == 1 && !v[i][j]) {
 					line = checkLine(board, i, j);
 					answer = answer < line*line ? line*line : answer;
 				}
@@ -39,6 +42,7 @@ public class BigestSquare { // 가장 큰 정사각형 찾기
 			if(board[nr][nc] == 0) {
 				break;
 			}
+			v[nr][nc] = true;
 			nr++;
 			nc++;
 		}
