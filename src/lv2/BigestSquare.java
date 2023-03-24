435e866826ac8b3baf6e4ae2ab62b0124724aaae
이소영 <th42500@naver.com>
2023-03-24 13:22:34 +0900

    [Feat] 2023.03.24 - 가장 큰 정사각형 찾기 풀이완료

--- src/lv2/BigestSquare.java
+++ src/lv2/BigestSquare.java
@@ -1,11 +1,19 @@
 package lv2;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class BigestSquare { // 가장 큰 정사각형 찾기
 	public static void main(String[] args) {
 		BigestSquare main = new BigestSquare();
 
 		int[][] board = { { 0, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 0, 0, 1, 0 } };
 //		int[][] board = {{0, 0, 1, 1}, {1, 1, 1, 1}};
+//		int[][] board = {{0, 0, 0, 0}, {0, 0, 0, 0}};
+//		int[][] board = {{1, 1, 1, 1}, {1, 1, 1, 1}};
+//		int[][] board = {{1}};
+//		int[][] board = {{0,0,0,1,1,1},{0,0,0,1,1,1},{1,1,1,1,1,1},{1,1,1,1,1,1},{1,1,1,1,1,1},{1,1,1,1,1,1}};
+//		int[][] board = {{1,1,1},{1,0,1},{1,1,1}};
 
 		long start = System.currentTimeMillis();
 		System.out.println(main.solution(board));
@@ -13,40 +21,39 @@ public class BigestSquare { // 가장 큰 정사각형 찾기
 		System.out.println("걸린 시간 = " + (end - start));
 	}
 
-	static boolean[][] v;
 	private int solution(int[][] board) {
 		int answer = 0;
 		
-		int line = 0;
-		v = new boolean[board.length][board[0].length];
-		for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[i].length; j++) {
-				if(board[i][j] == 1 && !v[i][j]) {
-					line = checkLine(board, i, j);
-					answer = answer < line*line ? line*line : answer;
+		int[][] memo = new int[board.length][board[0].length];
+		
+		for (int i = 0; i < memo.length; i++) {
+			memo[i][0] = board[i][0];
+		}
+		for (int i = 0; i < memo[0].length; i++) {
+			memo[0][i] = board[0][i];
+		}
+		
+//		for (int k = 0; k < memo.length; k++) {
+//			System.out.println(Arrays.toString(memo[k]));
+//		}
+		
+		if(memo.length <= 1) return memo[0][0];
+		
+		for (int i = 1; i < board.length; i++) {
+			for (int j = 1; j < board[i].length; j++) {
+				if(board[i][j] == 1) {
+					memo[i][j] = Math.min(memo[i-1][j-1], Math.min(memo[i-1][j], memo[i][j-1])) + 1;
+					if(memo[i][j] > answer) {
+						answer = memo[i][j];
+					}
+					System.out.println(answer);
 				}
-//				for (int k = 0; k < v.length; k++) {
-//					System.out.println(Arrays.toString(v[k]));
+//				for (int k = 0; k < memo.length; k++) {
+//					System.out.println(Arrays.toString(memo[k]));
 //				}
 			}
 		}
 		
-		return answer;
-	}
-	
-	private int checkLine(int[][] board, int sr, int sc) {
-		int nr = sr;
-		int nc = sc;
-		
-		while(nr < board.length && nc < board[0].length) {
-			if(board[nr][nc] == 0) {
-				break;
-			}
-			v[nr][nc] = true;
-			nr++;
-			nc++;
-		}
-		
-		return nr-sr;
+		return answer * answer;
 	}
 }
