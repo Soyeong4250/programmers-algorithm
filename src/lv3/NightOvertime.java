1580adedbe903ab454f52a2bc1e1f2abf75850f6
이소영 <th42500@naver.com>
2023-03-29 12:15:32 +0900

    [Feat] 2023.03.29 - 야근 지수 풀이중
    👉 정확성 테스트 모두 통과
    👉 효율성 테스트 실패

---
+++ src/lv3/NightOvertime.java
@@ -0,0 +1,54 @@
+package lv3;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class NightOvertime { // 야근 지수
+
+	public static void main(String[] args) {
+		NightOvertime main = new NightOvertime();
+
+//		int[] works = { 4, 3, 3 };
+//		int n = 4;
+
+//		int[] works = {2, 1, 2};
+//		int n = 1;
+
+		int[] works = {1, 1};
+		int n = 3;
+
+		System.out.println(main.solution(works, n));
+	}
+
+	private int solution(int[] works, int n) {
+		int answer = 0;
+		
+		PriorityQueue<Integer> time = new PriorityQueue<>(Collections.reverseOrder());
+		
+		
+		for (int i = 0; i < works.length; i++) {
+			System.out.println(works[i]);
+			time.add(works[i]);
+		}
+		
+		while(n > 0 && !time.isEmpty()) {
+			int t = time.poll();
+			System.out.println(t);
+			if(t > 0) {
+				t--;
+				time.add(t);
+				n--;
+			}
+		}
+		
+		while(!time.isEmpty()) {
+			int t = time.poll();
+			System.out.println(t);
+			answer += Math.pow(t, 2);
+			System.out.println("answer = " + answer);
+		}
+		
+		return answer;
+	}
+
+}
