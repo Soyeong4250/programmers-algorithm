12e7f48017a13953e90e90f9063433efcae5b829
이소영 <th42500@naver.com>
2023-09-19 00:06:04 +0900

    [Feat] 2023.09.18 - 다트 게임 풀이중

--- src/lv1/DartGame.java
+++ src/lv1/DartGame.java
@@ -2,13 +2,22 @@ package lv1;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class DartGame {  // 다트 게임 
 
 	public static void main(String[] args) {
-//		String dartResult = "1S2D*3T";
-		String dartResult = "1D2S#10S";
+		
+		// (1¹ * + 2³) * 2 + 3³		
+		String dartResult = "1S2D*3T";
+		
+		// 1² + 2¹ * (-1) + 10¹
+//		String dartResult = "1D2S#10S";
+		
+		// 1² + 2¹ + 0³
 //		String dartResult = "1D2S0T";
+		
+		// (1¹ * 2 + 2³ * 2) * 2 + 3¹
 //		String dartResult = "1S*2T*3S";
 //		String dartResult = "1D#2S*3S";
 //		String dartResult = "1T2D3D#";
@@ -20,26 +29,42 @@ public class DartGame {  // 다트 게임
 	private static int solution(String dartResult) {
 		int answer = 0;
 		
-		List<String> list = new ArrayList<>();
-		StringBuilder sb = new StringBuilder();
-		
-		int cate = -1;  // 0: 숫자, 1: STD. 2: *, #
+		int sum = 0;
+		double number = 0;
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < dartResult.length(); i++) {
-			String ch = dartResult.substring(i, i+1);
-			if(ch.matches("[0-9]")) {
-				if(cate != 0) {
-					list.add(ch);
-					cate = 0;
-				}				
-			} else if(ch.matches("[A-Z]")) {
-				if(cate != 1) {
-					list.add(ch);
-					cate = 0;
+			char ch = dartResult.charAt(i);
+			
+			if(Character.isDigit(ch)) {
+				sum += number;
+				number = 0;
+				System.out.println("isNumber");
+				sb.append(ch);
+			} else if('A' <= ch && ch <= 'Z') {
+				if(!sb.isEmpty()) {
+					number = Integer.parseInt(sb.toString());
+					sb.setLength(0);  // 초기화
+					
+					switch(ch) {
+					case 'D' : 
+						number = Math.pow(number, 2);
+						break;
+					case 'T' : 
+						number = Math.pow(number, 3);
+						break;
+					}
 				}
+			} else if (ch == '*') {
+				sum += number;
+				sum *= 2;
+			} else if (ch == '#') {
+				number *= (-1);
+				sum += number;
 			}
+			System.out.println("ch = " + ch + ", sum = " + sum);
 		}
 		
-		return answer;
+		
+		return sum;
 	}
 }
