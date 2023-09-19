63a9ea6454aa57535693ac55d1e7ec0a6a721b10
이소영 <th42500@naver.com>
2023-09-19 17:09:55 +0900

    [Feat] 2023.09.19 - 스킬 트리 풀이 완료

--- src/lv2/SkillTree.java
+++ src/lv2/SkillTree.java
@@ -1,5 +1,8 @@
 package lv2;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class SkillTree { // 스킬 트리
 
 	public static void main(String[] args) {
@@ -14,25 +17,43 @@ public class SkillTree { // 스킬 트리
 
 	private int solution(String skill, String[] skill_trees) {
 		int answer = 0;
-		StringBuilder skillStr = new StringBuilder();
-		
-		for (int i = 0; i < skill_trees.length; i++) {
-			skillStr.delete(0, skillStr.length());
-			String finalStr = skill.substring(skill.length()-1);
-			for (int j = 0; j < skill_trees[i].length()-1; j++) {
-				String s = skill_trees[i].substring(j, j+1);
-				if(skill.contains(s)) {
-					skillStr.append(s);
-				}
-				if(finalStr.equals(s)) {
-					break;
-				}
-			}
-			System.out.println(skillStr);
-			
-			if()
+
+		HashMap<Character, Integer> skillMap = new HashMap<>();
+		for (int i = 0; i < skill.length(); i++) {
+			skillMap.put(skill.charAt(i), i);
 		}
-		
+
+		boolean[] check = new boolean[skillMap.size()];
+
+		for (int t=0; t<skill_trees.length; t++) {
+			Arrays.fill(check, false);
+
+			boolean possible = true;
+			System.out.println(skill_trees[t]);
+			for (int i = 0; i < skill_trees[t].length(); i++) {
+				char ch = skill_trees[t].charAt(i);
+
+				if(skillMap.containsKey(ch)) {
+					System.out.println(ch);
+					int idx = skillMap.get(ch);
+
+					if(idx > 0) {
+						if (!check[idx-1]) {
+							possible = false;
+							System.out.println("불가능");
+							break;
+						}
+					}
+					check[idx] = true;
+				}
+				System.out.println(Arrays.toString(check));
+			}
+
+			if(possible) {
+				answer += 1;
+			}
+		}
+
 		return answer;
 	}
 
