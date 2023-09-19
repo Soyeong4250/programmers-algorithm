377ce8cba3341676d67672d6647066341e57f21a
이소영 <th42500@naver.com>
2023-09-19 17:26:27 +0900

    [Refactor] 2023.09.19 - 스킬트리 리팩토링

--- src/lv2/SkillTree.java
+++ src/lv2/SkillTree.java
@@ -1,9 +1,8 @@
 package lv2;
 
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
 
-public class SkillTree { // 스킬 트리
+public class SkillTree { // 스킬트리
 
 	public static void main(String[] args) {
 		SkillTree main = new SkillTree();
@@ -13,6 +12,33 @@ public class SkillTree { // 스킬 트리
 		String[] skill_trees = { "BACDE", "CBADF", "AECB", "BDA" };
 
 		System.out.println(main.solution(skill, skill_trees));
+		System.out.println(main.solution1(skill, skill_trees));
+	}
+
+	public int solution1(String skill, String[] skill_trees) {
+		int answer = 0;
+
+		List<String> skillTrees = new ArrayList<>(Arrays.asList(skill_trees));
+		for (String str : skillTrees) {
+			System.out.println("str = " + str);
+		}
+
+		Iterator<String> it = skillTrees.iterator();
+
+		// hasNext() -> next() -> remove()
+		while (it.hasNext()) {
+			String str = it.next();
+			System.out.println("it.next() = " + str);
+			System.out.println(str.replaceAll("[^" + skill + "]", ""));  // skill에 포함되지 않은 문자 제거
+			System.out.println(skill.indexOf(str.replaceAll("[^" + skill + "]", "")));  // skill에서 문자열 시작 인덱스 구하기
+			if (skill.indexOf(str.replaceAll("[^" + skill + "]", "")) != 0) {
+				it.remove();  // skill에 해당하지 않는 문자열 제거
+			}
+		}
+
+		answer = skillTrees.size();
+
+		return answer;
 	}
 
 	private int solution(String skill, String[] skill_trees) {
